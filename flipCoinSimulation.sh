8bf75024b4c87f50228e8666e9fbcb911d0e749d
vismayatk002 <vismayatk002@gmail.com>
2021-06-04 21:06:42 +0530

    Updated :If its tie then the game continues till the difference of minimum 2 points is achieved

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -25,4 +25,27 @@ then
 	echo "Tail Won by :" $point
 else
 	echo "Head and Tail are in Tie"
+	diffHeadTail=$(($headCount - $tailCount))
+	diffTailHead=$(($tailCount - $headCount))
+	while [[ $diffHeadTail -lt 2 && $diffTailHead -lt 2 ]]
+	do
+		randomnumber=$((RANDOM%2))
+		if [ $randomnumber -eq $head ]
+		then
+			((headCount++))
+		else
+			((tailCount++))
+		fi
+		diffHeadTail=$(($headCount - $tailCount))
+		diffTailHead=$(($tailCount - $headCount))
+	done
+	echo "Head Count :" $headCount
+	echo "Tail Count :" $tailCount
+	if [ $headCount -gt $tailCount ] 
+	then
+		echo "Head Wins"
+	else
+		echo "Tail Wins"
+	fi
 fi
+
