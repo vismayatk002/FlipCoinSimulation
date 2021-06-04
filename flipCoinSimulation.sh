71ecb2d2eb58bc8967331a554e8f3419f5b8d5b6
vismayatk002 <vismayatk002@gmail.com>
2021-06-04 19:05:21 +0530

    Updated: counted number of Head and Tail has won

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 head=1
 tail=0
-randomnumber=$((RANDOM%3))
-if [ $randomnumber -eq $head ]
-then
-	echo "Head as Winner"
-else
-	echo "Tail as Winner"
-fi
+headCount=0
+tailCount=0
+for ((i=0; i<10; i++))
+do
+	randomnumber=$((RANDOM%3))
+	if [ $randomnumber -eq $head ]
+	then
+		((headCount++))
+	else
+		((tailCount++))
+	fi
+done
+echo "Number of times Head Won :" $headCount
+echo "Number of times Tail Won :" $tailCount
